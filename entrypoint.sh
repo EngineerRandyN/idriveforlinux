946f398a3dce9b748eb67c5ce2f50655146997c0
engineerrandyn <engineerrandyn@outlook.com>
2024-02-06 12:29:28 -0600

    Modified entrypoint script to turn off CDP. Also changed CRLF to LF in all files for the docker image.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ IDRIVE_BIN_DIR="/opt/IDriveForLinux/bin"
 
 if [[ ! -d "$IDRIVE_BIN_DIR" ]]; then
     expect script.exp ${USERNAME} ${PASSWORD} ${BACKUP_NAME}
+    echo "1" > $IDRIVE_BIN_DIR/cdp.stop
 fi
 
 tail -f /dev/null
